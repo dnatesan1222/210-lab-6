4b0c3e9c39e6a955e02be37f33db2b2bc92e848a
Diksha <diksha@macbookpro>
2025-09-14 19:27:58 -0700

    enterArrayData() start

--- arrays.cpp
+++ arrays.cpp
@@ -1,2 +1,30 @@
 // COMSC-210-5470 | Lab 6 | Diksha Tara Natesan
 // IDE used: Vim
+
+#include <iostream>
+using namespace std;
+
+void enterArrayData(double *arr){
+    cout << "Data entry for the array:\n";
+    for (int i = 0; i < SIZE; i++){
+        cout << "   > Element #" << i << ": ";
+        cin >> arr[i];
+    }
+
+}
+
+int main(){
+    double *dynArr = nullptr;
+    dynArr = new double[SIZE];
+    enterArrayData(dynArr);
+    delete [] dynArr;
+}
+/*
+double *ptr = nullptr;
+ptr = new double;
+
+//dynamic array
+double *dynArr = nullptr;
+dynArr = new double[SIZE];
+
+delete [] dynArr;*/
