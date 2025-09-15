29e5b46590fcd89fb6c37683bd018d7430e1bdf9
Diksha <diksha@macbookpro>
2025-09-14 19:58:30 -0700

    main() and headers complete

--- arrays.cpp
+++ arrays.cpp
@@ -18,8 +18,9 @@ void enterArrayData(double *arr){
     cout << "Data entry complete." << endl;
 }
 
-
-
+// outputArrayData() prints the values stored in each element of the given dynamic array out on one line
+// arguments: double *arr (a dynamic array pointer)
+// returns: nothing
 void outputArrayData(double *arr){
     cout << "Outputting array elements:";
     for (int i = 0; i < SIZE; i++)
@@ -27,7 +28,9 @@ void outputArrayData(double *arr){
     cout << endl;
 }
 
-
+// sumArray() adds the values up from each element within the given dynamic array
+// arguments: double *arr (a dynamic array pointer)
+// returns: sum of the values stored in the dynamic array
 double sumArray(double *arr){
     double sum = 0;
     for (int i = 0; i < SIZE; i++)
@@ -35,15 +38,20 @@ double sumArray(double *arr){
     return sum;
 }
 
+// main() creates a dynamic array and passes it through the three functions created above then deletes the memory once the array's use is complete
+// arguments: nothing
+// returns: nothing
 int main(){
     double *dynArr = nullptr;
     dynArr = new double[SIZE];
     enterArrayData(dynArr);
     outputArrayData(dynArr);
-    cout << sumArray(dynArr) << endl;
+    cout << "Sum of values: " << sumArray(dynArr) << endl;
     delete [] dynArr;
 }
-/*
+
+
+/* notes/basic reference from the lesson 
 double *ptr = nullptr;
 ptr = new double;
 
