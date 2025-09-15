0e9c7dfbae83f6889f56cb0acdd2eed1b9958a76
Diksha <diksha@macbookpro>
2025-09-14 19:37:44 -0700

    update

--- arrays.cpp
+++ arrays.cpp
@@ -4,18 +4,22 @@
 #include <iostream>
 using namespace std;
 
+
+// enterArrayData() asks for user input to populate the given dynamic array
+// arguments: double *arr (a dynamic array pointer)
+// returns: nothing
 void enterArrayData(double *arr){
     cout << "Data entry for the array:\n";
-    for (int i = 0; i < SIZE; i++){
+    for (int i = 0; i < 5; i++){ //how do i make it so i < size when size is NOT 5
         cout << "   > Element #" << i << ": ";
         cin >> arr[i];
     }
-
+    cout << "Data entry complete." << endl;
 }
 
 int main(){
     double *dynArr = nullptr;
-    dynArr = new double[SIZE];
+    dynArr = new double[5];
     enterArrayData(dynArr);
     delete [] dynArr;
 }
